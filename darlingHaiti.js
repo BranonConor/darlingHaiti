a0d575183988ae1909e4a15e18ab6bfd7637f369
Branon Eusebio <50970682+BranonConor@users.noreply.github.com>
2019-07-18 16:41:45 -0700

    JS Code Update
    - Removed hover animation functions and applied them in CSS instead (far less code)

--- darlingHaiti.js
+++ darlingHaiti.js
@@ -1,17 +1,3 @@
 AOS.init();
 
-function contentHover() {
-	var content = document.getElementsByTagName("i");
-	for(var i = 0; i < content.length; i++) {
-		content[i].addEventListener("mouseover", function() {
-			this.classList.add("brandHover");
-			this.classList.remove("rotateOut");
-		});
-		content[i].addEventListener("mouseout", function() {
-			this.classList.remove("brandHover");
-			this.classList.add("rotateOut");
-		});
-	}
-}
-contentHover();
 
