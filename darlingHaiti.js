c8bd3ef60640cbd4cf1e687e188bca39caca533c
Branon Eusebio <50970682+BranonConor@users.noreply.github.com>
2019-08-01 11:18:12 -0700

    Updated Form with DOM
    - Added DOM manipulation for form elements - now requires input for submission
    - Need: selective input - if they don't add email, require email specifically, and same for message

--- darlingHaiti.js
+++ darlingHaiti.js
@@ -1,3 +1,12 @@
 AOS.init();
 
-
+//Require input before submitting form
+const formButton = document.querySelector('#submit');
+formButton.addEventListener('click', function() {
+	if (document.querySelector('#email').value && document.querySelector('#message').value) {
+		console.log("Email Entered!"); 
+		console.log("Message Entered!");
+	} else {
+		alert ('Please fill in each form item before submitting.');
+	}
+}); 
